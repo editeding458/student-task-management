36d6d4f8a723a2083b0c21afcd6616b415d58523
editeding <editediting458@gmail.com>
2026-01-30 13:48:55 +0530

    [AN][added]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Register from './pages/Register'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,8 +9,10 @@ function App() {
   return (
     <>
       <Header/>
+      <Register/>
+  
     </>
-    
+
   )
 }
 
---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+
+const Register = () => {
+  return (
+    <>
+      <div className="from-container">
+       <h1>Register</h1>
+      </div>
+    </>
+  );
+};
+
+export default Register;
