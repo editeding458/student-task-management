6b0b3f631fc1109eff1e2a26955cc9d459012401
editeding <editediting458@gmail.com>
2026-02-03 09:10:33 +0530

    [AN][added]

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
+const DefaultRoute = () => {
+  const authData = JSON.parse(localStorage.getItem("authData"));
+  if (authData) {
+    return <Navigate to="/Login" replace />;
+  }
+  return <Navigate to="/Register" replace />;
+  
+
+};
 function App() {
   const route = createBrowserRouter([
-      {
+    {
       path: "/",
-      element: < Login/>
+      element: <DefaultRoute />,
     },
     {
       path: "/Login",
-      element: <Login />
+      element: <Login />,
     },
     {
       path: "/Register",
-      element: <Register />
+      element: <Register />,
+    },
+     {
+      path: "/dashboard",
+      element: <Dashboard />,
     },
   ]);
 
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import "./Dashboard.css";
+function Dashboard() {
+  return (
+    <div className="from-group">
+      <h1>This is Dashboard.</h1>
+    </div>
+  )
+}
+
+export default Dashboard
